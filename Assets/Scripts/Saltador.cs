1396e61ec0184f86fc9991aebb6e7b7cfdc3612a
bianca-soares <biancasoaresdasilva2016@gmail.com>
2021-01-26 12:06:31 -0300

    diamante animation

--- Assets/Scripts/Saltador.cs
+++ Assets/Scripts/Saltador.cs
@@ -19,38 +19,40 @@ public class Saltador : MonoBehaviour
     // Update is called once per frame 70 frame po segundo
     void Update()
     {
-        //if(Physics.OverlapSphere(CheckColisaoEsfera.position, 0.1f).Length == 0); //verifica colisão com a plataforma
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             spacePressionada = true;
-
-            if ((spacePressionada) && (naPlataforma))
-            {
-                referenciaRigidbody.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
-            }
-            
-            spacePressionada = false;
         }
 
         //Movimento horizontal
         eixoHorizontal = Input.GetAxis("Horizontal");
-
-        referenciaRigidbody.velocity = new Vector3(eixoHorizontal, referenciaRigidbody.velocity.y, 0);
-
     }
 
     // 100 por segundo
-    private void FixedUpdade()//para atualizações física
+    private void FixedUpdate()//para atualizações física
     {
- 
-        if (spacePressionada)
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+            print("Found an object - distance: " + hit.distance);
+
+
+        /*if(Physics.OverlapSphere(CheckColisaoEsfera.position, 0.1f).Length == 0) //verifica colisão com a plataforma
+        {
+            return;
+        }*/
+
+        if ((spacePressionada) && (naPlataforma))
         {
             Debug.Log("space pressionada");
          
-           //referenciaRigidbody.AddForce(Vector3.up*9, ForceMode.VelocityChange);
-            
+            referenciaRigidbody.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+
+            spacePressionada = false;  
         }
+
+        referenciaRigidbody.velocity = new Vector3(eixoHorizontal, referenciaRigidbody.velocity.y, 0);
 
     }
 
@@ -64,5 +66,11 @@ public class Saltador : MonoBehaviour
     {
         naPlataforma = false;
         Debug.Log("Sem colisão com a plataforma");
+    }
+
+    //Colisão para colatar objetos
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
     }
 }
