ceec65a56e239aa04ff620d0f8feca4a8eec9652
bianca-soares <biancasoaresdasilva2016@gmail.com>
2021-02-02 18:06:33 -0300

    Saltador

--- Assets/Scripts/Saltador.cs
+++ Assets/Scripts/Saltador.cs
@@ -9,9 +9,15 @@ public class Saltador : MonoBehaviour
     private Rigidbody referenciaRigidbody;
     private bool naPlataforma;
     private Transform CheckColisaoEsfera;
+    public float disMax;
+    public float forcaPulo;
+    public LayerMask layerMask;
+    public float raio;
 
+    //Máscara layer  usada ​​por raycasting para seletivamente criar colisões
+   
     // Start is called before the first frame update
-    void Start()
+       void Start()
     {
         referenciaRigidbody = GetComponent<Rigidbody>();
     }
@@ -19,9 +25,32 @@ public class Saltador : MonoBehaviour
     // Update is called once per frame 70 frame po segundo
     void Update()
     {
+        RaycastHit hit;// obter informações do raycast
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             spacePressionada = true;
+        }
+
+        if (Physics.SphereCast(transform.position, raio, -Vector3.up, out hit, 1, layerMask) && spacePressionada)
+        {                                       //raio da esfera/direção/info    /layer plataforma
+
+            var direcao = hit.point - transform.position;
+
+            var poxPlataforma = hit.distance < disMax;
+
+            direcao.Normalize();
+
+            var angulo = Vector3.Angle(-Vector3.up, direcao);
+
+            print("teste " + angulo);
+
+            if (angulo < 90 && poxPlataforma)
+            {
+                referenciaRigidbody.AddForce(Vector3.up * forcaPulo, ForceMode.VelocityChange);
+            }
+
+            spacePressionada = false;
         }
 
         //Movimento horizontal
@@ -32,13 +61,7 @@ public class Saltador : MonoBehaviour
     private void FixedUpdate()//para atualizações física
     {
 
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
-            print("Found an object - distance: " + hit.distance);
-
-
-        /*if(Physics.OverlapSphere(CheckColisaoEsfera.position, 0.1f).Length == 0) //verifica colisão com a plataforma
+        /* if(Physics.OverlapSphere((CheckColisaoEsfera.position, 0.1f).Length == 0) //verifica colisão com a plataforma
         {
             return;
         }*/
@@ -47,12 +70,20 @@ public class Saltador : MonoBehaviour
         {
             Debug.Log("space pressionada");
          
-            referenciaRigidbody.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            //referenciaRigidbody.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
 
-            spacePressionada = false;  
+             
         }
 
         referenciaRigidbody.velocity = new Vector3(eixoHorizontal, referenciaRigidbody.velocity.y, 0);
+    }
+
+    
+
+    private void OnDrawGizmos()//desenha linha criada
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position - Vector3.up * disMax);
 
     }
 
