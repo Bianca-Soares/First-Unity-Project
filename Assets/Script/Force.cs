aeee39d0db8def3188b2e483703e0e6d6cc3122e
bianca-soares <biancasoaresdasilva2016@gmail.com>
2021-02-08 00:08:10 -0300

    bonus, vida e game over

--- Assets/Script/Force.cs
+++ Assets/Script/Force.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Force : MonoBehaviour
 {
@@ -12,20 +13,34 @@ public class Force : MonoBehaviour
     private float eixoVertical;
     //valor para coordenada de x
     private float eixoHorizontal;
-    [SerializeField] public float velocidade;
+    [SerializeField] public float velocidade = .0f;
+    private string mensagem;
+    private string msg;
+    private int vida = 5;
+    private int bonus = 0;
+    Vector3 posiInicial;
+    [SerializeField] public Text info;
+  
     void Start()
     {
         refRigidbody = GetComponent<Rigidbody>();//a ref recebeu o rigidbody do heroi
+
+        posiInicial = transform.position;//Editar para posição inicial
+        mensagem = "Início";
+
+        info.text = mensagem;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
 
     private void FixedUpdate()
     {
+        
         //quardar informação das teclas
         eixoHorizontal = Input.GetAxis("Horizontal");
         eixoVertical = Input.GetAxis("Vertical");
@@ -34,6 +49,65 @@ public class Force : MonoBehaviour
         Vector3 vetor = new Vector3(eixoHorizontal, 0.0f, eixoVertical);
 
         //adicionar força para o movimento
-        refRigidbody.AddForce(vetor * 10);//mais velocidade 
+        refRigidbody.AddForce(vetor * velocidade);//mais velocidade 
+
+        msg = "\n Vidas: " + vida.ToString() + "\n Bonus: " + bonus.ToString();
+         
+        info.text = mensagem + msg;
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Vida"))
+        {
+            mensagem = "Ganhou Vida";
+            other.gameObject.SetActive(false);
+            
+            vida++;
+        }
+
+        if (other.gameObject.CompareTag("Bonus"))
+        {
+            mensagem = "Ganhou Bônus";
+            other.gameObject.SetActive(false);
+            bonus = bonus + 10;
+        }
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision) {
+        
+        if (collision.gameObject.CompareTag("Star"))
+        {
+            inicio();
+            mensagem = "Perdeu Vida";
+
+            vida--;
+            
+            
+            if (vida <= 0)
+            {
+                mensagem = "Fim de Jogo";
+                msg = "\nVidas: " + vida.ToString() + "\nBonus: " + bonus.ToString();
+                info.text = mensagem + msg;
+                this.gameObject.SetActive(false);
+                
+            }
+        }
+
+    }
+
+    private void inicio()
+    {
+
+        posiInicial.x = 0.0f;
+        posiInicial.y = 0.0f;
+        posiInicial.z = 0.0f;
+
+        transform.position = posiInicial;
+    }
+
 }
